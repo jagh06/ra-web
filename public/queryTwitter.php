72cde970b761eecf1a12fe58f90339ba00fa9610
jagh06 <jhonguzmanhernandez659@gmail.com>
2023-07-14 16:12:00 -0600

    Subir proyecto

---
+++ public/queryTwitter.php
@@ -0,0 +1,63 @@
+<?php
+
+function queryTwitter($search)
+{
+	$url = "https://api.twitter.com/1.1/search/tweets.json";
+	if($search != "")
+		//
+		$search = "#".$search;
+	$query = array( 'count' => 100, 'q' => urlencode($search), "result_type" => "recent");
+    $oauth_access_token = "1455285575842402318-WZPYRj1aSsaBnIH78ZO3AsKWWuQwCK";
+    $oauth_access_token_secret = "RY4MXvyP03MhTycRi71DpbYJNyoBPWKbk0wpy0qtv9KU2";
+    $consumer_key = "01DEOQ5M7rnfMqn5iz0zaU1lX";
+    $consumer_secret = "LFxtKV2kqwL1TESASAYgjHBQPzdQviDYbTWScU0zR0x3J24yH4";
+
+	$oauth = array(
+					'oauth_consumer_key' => $consumer_key,
+					'oauth_nonce' => time(),
+					'oauth_signature_method' => 'HMAC-SHA1',
+					'oauth_token' => $oauth_access_token,
+					'oauth_timestamp' => time(),
+					'oauth_version' => '1.0');
+
+	$base_params = empty($query) ? $oauth : array_merge($query,$oauth);
+	$base_info = buildBaseString($url, 'GET', $base_params);
+	$url = empty($query) ? $url :  $url . "?" . http_build_query($query);
+
+	$composite_key = rawurlencode($consumer_secret) . '&' . rawurlencode($oauth_access_token_secret);
+	$oauth_signature = base64_encode(hash_hmac('sha1', $base_info, $composite_key, true));
+	$oauth['oauth_signature'] = $oauth_signature;
+
+	$header = array(buildAuthorizationHeader($oauth), 'Expect: ');
+	$options = array( CURLOPT_HTTPHEADER => $header,
+					  CURLOPT_HEADER => false,
+				   	  CURLOPT_URL => $url,
+					  CURLOPT_RETURNTRANSFER => true,
+					  CURLOPT_SSL_VERIFYPEER => false);
+
+	$feed = curl_init();
+	curl_setopt_array($feed, $options);
+	$json = curl_exec($feed);
+	curl_close($feed);
+	return $json;
+}
+
+function buildBaseString($baseURI, $method, $params)
+{
+    $r = array();
+    ksort($params);
+    foreach($params as $key=>$value){
+    	$r[] = "$key=" . rawurlencode ($value);
+	}
+	return $method."&" . rawurlencode($baseURI) . '&' . rawurlencode(implode('&', $r));
+}
+function buildAuthorizationHeader($oauth)
+{
+    $r = 'Authorization: OAuth ';
+   	$values = array();
+    foreach ($oauth as $key=>$value)
+        $values[] = "$key=\"" . rawurlencode($value) . "\"";
+    $r .= implode (', ', $values);
+    return $r;
+}
+?>
